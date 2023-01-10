d818170c250119385c7e676c5bbad9593aee02ee
Shaunson26 <Shaunson26@users.noreply.github.com>
2023-01-10 10:14:05 +0000

    Deploying to gh-pages from @ Shaunson26/HL7.R@c60cfdd529ea4d2ad292f593948ff981fac34d75 🚀

---
+++ reference/figures/hexsticker.R
@@ -0,0 +1,16 @@
+imgurl <- "hl7-envelope.png"
+sticker(
+  imgurl,
+  # title
+  package = "",
+  # image
+  s_x = 1,
+  s_y = 1,
+  s_width = 0.75,
+  s_height = 0.75,
+  # border
+  h_size	= 1.2,
+  h_fill	= "#002664",
+  h_color = "#d7153a",
+  filename = 'man/figures/HL7.R-logo.png'
+)
